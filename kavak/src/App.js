88fd8b689218178d4bb5dc65b56c196f0f871f54
AlanTaplu140300 <180300359@ucaribe.edu.mx>
2021-03-18 21:53:33 -0500

    vehicle card add

--- kavak/src/App.js
+++ kavak/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 import TestForm from './components/TestForm';
+import VehicleCard from './components/VehicleCard';
 import {Container, Row, Col} from 'react-bootstrap';
 
+async function test() {
+  const response = await fetch('http://127.0.0.1:8000/api/car/');
+  const data = await response.json();
+
+  // console.log(data.results);
+  return data.results;
+}
 
 function App() {
+  test()
+  //fetch('http://127.0.0.1:8000/api/car/',{
+  //      'mode': 'cors',
+  //      'headers': {
+  //        'Access-Control-Allow-Origin': '*',
+  //      }      
+  //    })
+  //    .then(response => response.json())
+  //    .then(json => console.log(json))
+
   return (
     <Container>
       <Row>
@@ -11,7 +29,8 @@ function App() {
           <TestForm/>
         </Col>
         <Col>
-        
+          <VehicleCard
+          />
         </Col>
       </Row>
     </Container>
